1d85928e21d79f528ab1c7dc229354e304ef9407
Karan <karan2399@gmail.com>
2022-02-22 19:10:28 -0500

    Heroku

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/household/index.html'));
+        '/dist/index.html'));
 });
 app.listen(process.env.PORT || 8080);
